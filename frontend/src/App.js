8c35b133c31938408969f5f72675be15e522fec1
Dan Taylor <dantaylo@microsoft.com>
2019-09-22 00:33:52 +0000

    Revert to default App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // store users in a new variable
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    let fetchData = async () => {
-      let response = await fetch('/api/users')
-      let json = await response.json()
-      setUsers(json)
-    }
-    fetchData()
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Users</h1>
-        {users.map((user) => <p>
-          {user.username} {user.email}
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        )}
         <a
           className="App-link"
           href="https://reactjs.org"
